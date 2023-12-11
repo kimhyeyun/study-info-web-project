009d0463ea26078c47d64d24b868b5f5b1193c9c
Kimhyeyun <cqqwer@gmail.com>
2023-12-11 20:34:54 +0900

    init

---
+++ src/main/java/com/dev/studyinfoshareweb/StudyInfoShareWebApplication.java
@@ -0,0 +1,13 @@
+package com.dev.studyinfoshareweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyInfoShareWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyInfoShareWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/studyinfoshareweb/StudyInfoShareWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.studyinfoshareweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyInfoShareWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
